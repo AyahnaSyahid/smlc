e121fb64576719fc744abc7ada6d5db2da9d1bc1
AyahnaSyahid <ayah.syahid2017@gmail.com>
2025-10-21 07:32:11 +0700

    Attempt 1

--- py/smlc/playground.py
+++ py/smlc/playground.py
@@ -1,19 +1,20 @@
-from PySide6.QtNetwork import QTcpSocket, QUdpSocket, QTcpServer, QHostAddress
+from PySide6.QtNetwork import QTcpSocket, QUdpSocket, QTcpServer, QHostAddress, QAbstractSocket
 from PySide6.QtCore import QCoreApplication, QTimer, QThread, QMutex, QMutexLocker, \
-                                Slot, Signal, QDateTime
+                                Slot, Signal, QDateTime, QObject
 import json
 import logging
 
 logger = logging.getLogger(__name__)
 
 class ConnectionHanlder(QObject):
+    pass
     
 class Node(QObject):
     peerDiscovered = Signal(str)
     
     def __init__(self, node_name, parent=None):
         super().__init__(parent)
-        self.mutex = QMutex(self)
+        # self.mutex = QMutex(self)
         self.node_id = node_name
         self.tcpServer = QTcpServer(self)
         self.udpSocket = QUdpSocket(self)
@@ -21,14 +22,14 @@ class Node(QObject):
         self.m_groups = ['224.1.1.1']
         self._local_ip_cache = {'ip':None, 'last_check':None}
         self.annouce_timer = QTimer(self)
-        self.annouce_timer.setInterval(80000)
         self.annouce_timer.timeout.connect(self.send_announce)
+        self.annouce_timer.setInterval(5000)
+        self.annouce_timer.start()
         self.peers = {} # node_id: ip_addr, port_addr, last_seen
         self.peers_mutex = QMutex(self)
-        
 
     def localIp(self):
-        if self._loca_lip_cache['ip'] is None:
+        if self._local_ip_cache['ip'] is None:
             s = QUdpSocket()
             s.connectToHost('8.8.8.8', 80)
             l_ip = s.localAddress().toString()
@@ -39,66 +40,89 @@ class Node(QObject):
             self._local_ip_cache = {'ip':None, 'last_check':None}
             return self.localIp()
         else:
-            return self._local_lip_cache['ip']
+            return self._local_ip_cache['ip']
     
+    @Slot(QAbstractSocket.SocketError)
+    def onError(self, err):
+        logger.warning(f"{self.node_id} ERROR {err}")
     
     @Slot()
     def send_announce(self):
         if not self.started:
             logger.warning("ERROR server not started yet")
             return 
+        logger.info(f"{self.node_id} >> Sending Payload")
         payload = {'message_type':'announce', 
                    'node_id':self.node_id,
                    'ip_addr':self.localIp(),
-                   'port_addr':self.server.serverPort() }
+                   'port_addr':self.tcpServer.serverPort() }
         data = json.dumps(payload)
         for group in self.m_groups:
-            self.udpSocket.writeDatagram(data.encode(), group, 55655)
-        self.udpSocket.writeDatagram(data.encode(), '255.255.255.255', 55655)
+            self.udpSocket.writeDatagram(data.encode(), QHostAddress(group), 55655)
+        # self.udpSocket.writeDatagram(data.encode(), QHostAddress('255.255.255.255'), 55655)
 
     @Slot()
     def start(self):
         if self.started:
             return
         self.started = True
-        self.server.listen(QHostAddress.SpecialAddress.AnyIPv4)
-        self.udpSocket.bind(QHostAddress.SpecialAddress.AnyIPv4, 55655)
-        self.udpSocket.joinMulticastGroup('224.1.1.1')
-        self.server.newConnection.connect(self.handleConnection)
+        logger.info(f"{self.node_id} >> Starting")
+        self.tcpServer.acceptError.connect(self.onError)
+        self.udpSocket.errorOccurred.connect(self.onError)
+        self.tcpServer.listen(QHostAddress.SpecialAddress.AnyIPv4)
+        self.udpSocket.bind(QHostAddress.SpecialAddress.AnyIPv4, 55655, QAbstractSocket.BindFlag.ReuseAddressHint)
+        self.udpSocket.joinMulticastGroup(QHostAddress('224.1.1.1'))
+        self.tcpServer.newConnection.connect(self.handleConnection)
         self.udpSocket.readyRead.connect(self.readDatagram)
     
     @Slot()
     def readDatagram(self):
+        logger.info(f"{self.node_id} >> Receiving Datagrams")
         while self.udpSocket.hasPendingDatagrams():
-            data, host, port = self.udpSocket.readDatagram(-1)
+            dg = self.udpSocket.receiveDatagram()
+            data = dg.data().data()
+            logger.debug(f"{data}")
             payload = None
             try:
                 payload = json.loads(data.decode())
             except Exception as ex:
-                log.info("Gagal menerima payload")
+                logger.info(f"Gagal menerima payload {ex}")
                 return
-            if payload is not None:
-                if paylode["message_type"] == 'announce':
-                    if payload['node_id'] == self.node_id:
-                        return
-                    if payload['node_id'] not in self.peers.keys()
-                        lock = QMutexLocker(self.peers_mutex)
-                        peer_id = payload['node_id']
-                        peers[node_id] = (payload['ip_addr'], payload[port_addr], QDateTime.currentDateTime())
-                        return
+            if payload["message_type"] == 'announce':
+                peer_node_id = payload["node_id"]
+                peer_ip_addr = payload["ip_addr"]
+                peer_port_addr = payload["port_addr"]
+                
+                if payload['node_id'] == self.node_id:
+                    return
+                lock = QMutexLocker(self.peers_mutex)
+                if payload['node_id'] not in self.peers.keys():
+                    logger.info(f"Discovered peer [{peer_node_id}]@{peer_ip_addr}:{peer_port_addr}")
+                    peer_id = payload['node_id']
+                    self.peers[node_id] = (payload['ip_addr'], payload[port_addr], QDateTime.currentDateTime())
+                    return
+
     @Slot()
     def stop(self):
         if not self.started:
             return
-        self.server.close()
+        self.tcpServer.close()
         self.udpSocket.abort()
         self.started = False
 
     @Slot()
     def handleConnection(self):
-        while self.server.hasPendingConnection():
-            con = self.server.nextPendingConnection()
+        while self.tcpServer.hasPendingConnection():
+            con = self.tcpServer.nextPendingConnection()
             con.abort()
             con.deleteLater()
-    
 
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    app = QCoreApplication([])
+    node1 = Node("Node1")
+    node2 = Node("Node2")
+    node1.start()
+    node2.start()
+    QTimer.singleShot(60000, app.quit)
+    app.exec()
